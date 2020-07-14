9ea295f01797108a365766d8758ebc3542c3f237
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-14 19:41:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,9 +33,6 @@ class Song
     self.all.detect { |song| song.name == song.name}
   end
   
-  def self.find_or_create_by_name(song_name)
-    self.find_or_create_by_name(song_name) || self.find_or_create_by_name(song_name)
-  end
   
     def self.find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create_by_name(song_name)
