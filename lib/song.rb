b5b4beba944683a5008279a1541a7e5fe69abff1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-14 19:20:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,18 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    new_song = self.new
+    @@all << new_song
+    new_song
+  end
 end
