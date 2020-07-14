abbb850868110a8ea503eeef4f9a512209daaa46
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-14 19:34:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,8 +32,5 @@ class Song
   def self.find_by_name(song_name)
     self.all.detect { |song| song.name == song.name}
   end
-  def self.find_by_name(song_name)
-    self.all.detect { |song| song.name == song_name}
-  end
 
 end
